1d49eef030aa518f0fe1ccae1b50386874d1154c
Carlos García Murillo <c.g.murillo91@gmail.com>
2023-02-05 18:14:38 +0100

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PRÁCTICA_4._2.Casting_de_números
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroEntero = int.Parse(textBox1.Text);
+            int doble = numeroEntero * 2;
+            int mitad = numeroEntero / 2;
+
+            textBox3.Text = doble.ToString();
+            textBox4.Text = mitad.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float numeroReal = float.Parse(textBox2.Text);
+            float doble = numeroReal * 2;
+            float mitad = numeroReal / 2;
+
+            textBox3.Text = doble.ToString();
+            textBox4.Text = mitad.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
